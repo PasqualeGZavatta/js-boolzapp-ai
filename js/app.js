1a20f5af0153e7b5dce7e14f6a6a9a0291ba2056
PasqualeGZavatta <pasqualeg.zavatta@gmail.com>
2026-09-13 14:47:17 +0200

    Funzione addTextMessage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+const myMessageEl = document.getElementById('new-text-box')
 
+const inputMessageEl = document.getElementById('my-text')
+const form = document.querySelector('form')
+
+
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    addTextMessage()
+
+})
+
+
+//funcions
+
+function addTextMessage() {
+
+    if (inputMessageEl.value.trim() !== '') {
+        myMessageEl.innerHTML += `<p>${inputMessageEl.value}</p>`
+        inputMessageEl.value = ''
+    }
+
+}
